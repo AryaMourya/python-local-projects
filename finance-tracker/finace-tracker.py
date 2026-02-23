af8d9fe2ab7a9aa51fe751bcbf592dcb50f677a5
AryaMourya <aryamourya0@gmail.com>
2026-02-23 21:13:13 +0530

    add initial implementation of expense tracker

---
+++ finance-tracker/finace-tracker.py
@@ -0,0 +1,10 @@
+print("Welcome to Expense Tracker")
+print("====MENU====")
+print ("1.Add Expense")
+print("2.View All Expenses")
+print("3.View Total Spending")
+print("4.Exit ")
+print("============")
+
+Choice = int(input("Enter your choice (1-4)"))
+
